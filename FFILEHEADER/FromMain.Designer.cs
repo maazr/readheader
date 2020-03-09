564a971f66eb291aa914620e541bb836fa5970f5
Maaz Rizki <maaz.rizki@outlook.com>
2020-03-08 22:24:38 -0400

    Add files via upload

---
+++ FFILEHEADER/FromMain.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace FFILEHEADER
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxSrc = new System.Windows.Forms.ListBox();
+            this.listBoxChng = new System.Windows.Forms.ListBox();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.buttonReadFile = new System.Windows.Forms.Button();
+            this.buttonProcess = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.openFD = new System.Windows.Forms.OpenFileDialog();
+            this.tbSelectedFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelcolcount = new System.Windows.Forms.Label();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.listControl = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBoxSrc
+            // 
+            this.listBoxSrc.FormattingEnabled = true;
+            this.listBoxSrc.ItemHeight = 20;
+            this.listBoxSrc.Location = new System.Drawing.Point(24, 81);
+            this.listBoxSrc.Name = "listBoxSrc";
+            this.listBoxSrc.Size = new System.Drawing.Size(279, 584);
+            this.listBoxSrc.TabIndex = 0;
+            // 
+            // listBoxChng
+            // 
+            this.listBoxChng.FormattingEnabled = true;
+            this.listBoxChng.ItemHeight = 20;
+            this.listBoxChng.Location = new System.Drawing.Point(351, 81);
+            this.listBoxChng.Name = "listBoxChng";
+            this.listBoxChng.Size = new System.Drawing.Size(279, 284);
+            this.listBoxChng.TabIndex = 0;
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.ItemHeight = 20;
+            this.listBoxResult.Location = new System.Drawing.Point(677, 81);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(279, 584);
+            this.listBoxResult.TabIndex = 0;
+            // 
+            // buttonReadFile
+            // 
+            this.buttonReadFile.Location = new System.Drawing.Point(24, 23);
+            this.buttonReadFile.Name = "buttonReadFile";
+            this.buttonReadFile.Size = new System.Drawing.Size(125, 32);
+            this.buttonReadFile.TabIndex = 1;
+            this.buttonReadFile.Text = "Read File";
+            this.buttonReadFile.UseVisualStyleBackColor = true;
+            this.buttonReadFile.Click += new System.EventHandler(this.buttonReadFile_Click);
+            // 
+            // buttonProcess
+            // 
+            this.buttonProcess.Location = new System.Drawing.Point(191, 23);
+            this.buttonProcess.Name = "buttonProcess";
+            this.buttonProcess.Size = new System.Drawing.Size(125, 32);
+            this.buttonProcess.TabIndex = 1;
+            this.buttonProcess.Text = "Process";
+            this.buttonProcess.UseVisualStyleBackColor = true;
+            this.buttonProcess.Click += new System.EventHandler(this.buttonProcess_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(361, 23);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(125, 32);
+            this.buttonExit.TabIndex = 1;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // openFD
+            // 
+            this.openFD.DefaultExt = "csv";
+            // 
+            // tbSelectedFile
+            // 
+            this.tbSelectedFile.Location = new System.Drawing.Point(153, 677);
+            this.tbSelectedFile.Name = "tbSelectedFile";
+            this.tbSelectedFile.ReadOnly = true;
+            this.tbSelectedFile.Size = new System.Drawing.Size(803, 26);
+            this.tbSelectedFile.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 680);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Selected File:";
+            // 
+            // labelcolcount
+            // 
+            this.labelcolcount.AutoSize = true;
+            this.labelcolcount.Location = new System.Drawing.Point(509, 29);
+            this.labelcolcount.Name = "labelcolcount";
+            this.labelcolcount.Size = new System.Drawing.Size(121, 20);
+            this.labelcolcount.TabIndex = 4;
+            this.labelcolcount.Text = "# of column(s) : ";
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Location = new System.Drawing.Point(878, 34);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(52, 20);
+            this.labelCount.TabIndex = 5;
+            this.labelCount.Text = "Count";
+            // 
+            // listControl
+            // 
+            this.listControl.FormattingEnabled = true;
+            this.listControl.ItemHeight = 20;
+            this.listControl.Location = new System.Drawing.Point(351, 381);
+            this.listControl.Name = "listControl";
+            this.listControl.Size = new System.Drawing.Size(279, 284);
+            this.listControl.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 720);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.labelcolcount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbSelectedFile);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonProcess);
+            this.Controls.Add(this.buttonReadFile);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.listControl);
+            this.Controls.Add(this.listBoxChng);
+            this.Controls.Add(this.listBoxSrc);
+            this.Name = "FormMain";
+            this.Text = "Change File Header";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxSrc;
+        private System.Windows.Forms.ListBox listBoxChng;
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.Button buttonReadFile;
+        private System.Windows.Forms.Button buttonProcess;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.OpenFileDialog openFD;
+        private System.Windows.Forms.TextBox tbSelectedFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelcolcount;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.ListBox listControl;
+    }
+}
+
---
+++ FFILEHEADER/FromMain.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FFILEHEADER
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        private List<string> lst = new List<string>();
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonReadFile_Click(object sender, EventArgs e)
+        {
+            openFD.ShowDialog();
+            string filename = openFD.FileName;
+            tbSelectedFile.Text = filename;
+            readheader(filename);
+            loadtotable(listBoxSrc);
+            LoadChangeList();
+            LoadControlList();
+
+        }
+        private void LoadChangeList()
+        {
+            string sqlstr = "select Id,a.ColumnName,b.Cnt from fileColumns a inner join "
+            + "(select ColumnName,count(1) cnt from filecolumns group by ColumnName having count(ColumnName)>1) b "
+            + " on a.ColumnName = b.ColumnName order by a.ColumnName "; // this query give us reference id for update
+            string connstr = GetConnectionString();
+            listBoxChng.Items.Clear();
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = connstr;
+                conn.Open();
+                var da = new SqlDataAdapter(sqlstr, conn);
+                var dt = new DataTable();
+                string str;
+                da.Fill(dt);
+                foreach (DataRow row in dt.Rows)
+                {
+                    listBoxChng.Items.Add(row["ColumnName"].ToString());
+                }
+            }
+        }
+        private void LoadControlList()
+        {
+            string sqlstr = "select Distinct a.ColumnName from fileColumns a inner join "
+            + "(select ColumnName,count(1) cnt from filecolumns group by ColumnName having count(ColumnName)>1) b "
+            + " on a.ColumnName = b.ColumnName order by a.ColumnName"; // this query give us reference id for update
+            string connstr = GetConnectionString();
+            listControl.Items.Clear();
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = connstr;
+                conn.Open();
+                var da = new SqlDataAdapter(sqlstr, conn);
+                var dt = new DataTable();
+                da.Fill(dt);
+                foreach (DataRow row in dt.Rows)
+                {
+                    listControl.Items.Add(row["ColumnName"].ToString());
+
+                }
+            }
+        }
+
+        private void RenameColumn()
+        {
+            /* here we will number duplicate columns
+             * 
+             *
+             *
+            string chkstr;
+            string str;
+            // have to use Datatable, using reader was not successfull.
+            */
+            for (int i = 0; i < listControl.Items.Count; i++)
+            {
+
+                string sValue = listControl.Items[i].ToString();
+                int x = -1;
+                for (int j = 0; j < listBoxChng.Items.Count; j++)
+                {
+
+                    string chkstr = listBoxChng.Items[j].ToString();
+                    if (chkstr.Equals(sValue))
+                    {
+                        x++;
+                        if (x == 0)
+                        {
+                            listBoxResult.Items.Add(string.Format("{0}", listBoxChng.Items[j].ToString()));
+                        }
+                        else
+                        {
+                            listBoxResult.Items.Add(string.Format("{0}_{1}", listBoxChng.Items[j].ToString(), x.ToString()));
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+        private void updateHeader(string p1, string p2)
+        {
+            string updStr = "update filecolumns set ColumnName='" + p2 + "' where ID=" + Int32.Parse(p1);
+            string connstr = GetConnectionString();
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = connstr;
+                conn.Open();
+                var cmd = new SqlCommand(updStr, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+
+        }
+
+        private void loadtotable(ListBox listBoxSrc)
+        {
+            /* Here we load data from list to database tables
+             * 
+             */
+            string connectionString = GetConnectionString();
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+                string sqlTrunc = "TRUNCATE TABLE " + "filecolumns";
+                SqlCommand cmd = new SqlCommand(sqlTrunc, connection);
+                //string sqldel = "delete from filecolumns";
+                //SqlCommand cmd = new SqlCommand(sqldel, connection);
+                cmd.ExecuteNonQuery();
+                string sqlins = "insert into filecolumns (ColumnName) values(";
+                string ins;
+                foreach (var SrcItem in listBoxSrc.Items)
+                {
+                    ins = sqlins + "'" + SrcItem.ToString() + "')";
+                    cmd = new SqlCommand(ins, connection);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void readheader(string filename)
+        {
+            /* this is the key here is Microsoft VB library  
+             * 
+             * 
+             */
+
+            TextFieldParser par = new TextFieldParser(new StreamReader(@filename));
+            par.HasFieldsEnclosedInQuotes = true;
+            par.SetDelimiters(",");
+            string strline = "";
+            string[] _values = null;
+            int row = 0;
+            listBoxSrc.Items.Clear();
+            while (!par.EndOfData)
+            {
+
+                strline = "";
+                _values = par.ReadFields();
+                string fldstring;
+                if (row == 0)
+                {
+                    int x = 0;
+                    foreach (string field in _values)
+                    {
+                        x++;
+                        if (field.IndexOf("'") > 0) { fldstring = field.Replace("'", "''"); }
+                        else { fldstring = field; }
+                        if (fldstring == "") { fldstring = string.Format("fld{0}", Convert.ToString(x)); }
+                        listBoxSrc.Items.Add(fldstring);
+                        //listBoxChng.Items.Add(fldstring);
+                        //lst.Add(fldstring);
+                        //listBoxChng.DataSource = lst;
+
+                    }
+                    string colcount = string.Format("# of column(s) : {0}", Convert.ToString(x));
+                    labelcolcount.Text = colcount;
+                }
+                else
+                {
+                    break;
+                }
+                row++;
+
+
+            }
+            par.Close();
+        }
+
+        private void buttonProcess_Click(object sender, EventArgs e)
+        {
+            //processlist();
+            RenameColumn();
+        }
+
+        private void processlist()
+        {
+            // First try using list by self but did not work
+
+            int x = 0;
+            string Item1 = "";
+            string Item2 = "";
+            string newname = "";
+            //foreach (var ItemSrc in listBoxSrc.Items )
+            for (int i = 0; i < lst.Count; i++)
+            {
+                Item1 = lst[i].ToString();
+                newname = Item1;
+                //MessageBox.Show(newname);
+                //Item2 = ItemSrc.ToString();
+                x = -1;
+                listBoxResult.Items.Add(Item1);
+                for (int j = 0; j < listBoxChng.Items.Count; j++)
+                {
+                    Item2 = listBoxChng.Items[j].ToString();
+                    MessageBox.Show(Item1 + " " + Item2);
+                    if (Item1.Equals(Item2))
+                    {
+
+                        x++;
+                        if (x > 1)
+                        {
+
+                            listBoxResult.Items.Add(string.Format("{0}_{1}", Item2, x.ToString()));
+                        }
+                    }
+                    //lst.RemoveAt(0);
+                }
+                //lst.RemoveAt(0);
+            }
+        }
+        static private string GetConnectionString()
+        {
+            // To avoid storing the connection string in your code, 
+            // you can retrieve it from a configuration file.
+            // your database and connection.
+            return "Data Source=LT-MRIZKI;Initial Catalog=UTILS;User Id=nu;password=nu;"; // providerName=System.Data.SqlClient";
+        }
+    }
+}
